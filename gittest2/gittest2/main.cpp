06e6cf283a4ca1160c62993602b4bf81256c42d9
pzy <pengzhyong@qq.com>
2017-06-16 10:33:42 +0800

    reset to 2nd and add a anotation

--- gittest2/gittest2/main.cpp
+++ gittest2/gittest2/main.cpp
@@ -3,6 +3,6 @@ using namespace std;
 void main()
 {
 	cout << "the initial ver" << endl;
-	cout << "the second ver" << endl;
+	cout << "the second ver" << endl;//this is the reset ver to the 2nd ver
 
 }
